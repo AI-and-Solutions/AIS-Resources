32c86135f7633a9bec8fbc29cead2fbbf4cf0d8d
NovaJova17 <aguilj2@rpi.edu>
2024-10-11 20:19:30 -0400

    Updated Test
    Updated version of test file

--- venv-setup/test.py
+++ venv-setup/test.py
@@ -1,5 +1,10 @@
 from pathlib import Path
+from matplotlib import pyplot
+import numpy as np
 import requests
+import pickle
+import gzip
+import torch
 
 DATA_PATH = Path("data")
 PATH = DATA_PATH / "mnist"
@@ -12,3 +17,18 @@ FILENAME = "mnist.pkl.gz"
 if not (PATH / FILENAME).exists():
     content = requests.get(URL + FILENAME).content
     (PATH / FILENAME).open("wb").write(content)
+
+with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
+    ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding="latin-1")
+
+pyplot.imshow(x_train[0].reshape((28,28)), cmap="gray")
+
+try:
+    import google.colab
+
+except ImportError:
+    pyplot.show()
+print(x_train.shape)
+
+#x_train, y_train, x_valid, y_valid = map(torch.sensor, (x_train))
+
